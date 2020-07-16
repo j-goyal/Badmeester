0a111c7551df421886612602cdc1f0c66632b6be
Jatin <jating07925@gmail.com>
2020-07-16 17:32:06 +0530

    Added bell symbol, copyright symbol and also complete contact page layout

--- contact.php
+++ contact.php
@@ -9,13 +9,23 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <style>
-    #maincontainer {
-        min-height: 76vh;
-    }
+        i{
+            color: #339966;
+        }
+        h3{
+            line-height: 20%;
+        }
+        p{
+            margin-top:4%;
+            line-height:35%;
+            padding-bottom: 30px;
+            padding-left: 10px;
+        }
+      
     </style>
-
-
     <title>Contact Us</title>
 </head>
 
@@ -24,7 +34,72 @@
     <?php include 'partials/_header.php'; ?>
     <?php include 'partials/_dbconnect.php'; ?>
 
-    
+
+
+    <div class="container">
+        <div class="jumbotron jumbotron-sm" style="background-color:#339966;margin-top:2%;color:white;margin-right:-30px">
+            <div class="row">
+                <div class="col-sm-12 col-lg-12">
+                    <h1 class="h2" style="margin-top:-2%">
+                        Contact Us
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="well">
+                    <h3><i class="fa fa-home fa-1x"></i> Address:</h3>
+                    <p>Thapar University, P.O. Box 32, Patiala, Pin - 147004</p>
+                    
+                    <h3><i class="fa fa-envelope fa-1x"></i> E-Mail Address:</h3>
+                    <p>info@birisi.com</p>
+                    
+                    <h3><i class="fa fa-user fa-1x"></i> Phone Number</h3>
+                    <p>123456789</p>
+                    
+                    <h3><i class="fa fa-yelp fa-1x"></i> Support Center</h3>
+                    <p><a href="siteadresi.com/destek">siteadresi.com/destek</a>
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6" style="margin: 0px -10px; padding: 0px 0px">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13771.175861412305!2d76.3688073!3d30.3568049!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa7f6232cbef903a4!2sThapar+Institute+of+Engineering+and+Technology+University!5e0!3m2!1sen!2sin!4v1500631550669"
+                    width="600" height="430" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <h1 style="margin: 20px 0px;">Your Query:</h1>
+        <form method="post" action=''>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Enter your Name" required>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"
+                    required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter Your 10-digit Phone Number"
+                    required>
+            </div>
+
+            <div class="form-group">
+                <label for="content">Enter Message</label>
+                <textarea class="form-control" id="content" rows="8" name="content" required></textarea>
+            </div>
+            <button class="btn btn-success" type="submit">Submit</button>
+        </form>
+
+    </div>
+
 
 
     <?php include 'partials/_footer.html'; ?>
--- index.php
+++ index.php
@@ -118,14 +118,21 @@
     </div>
 
     <div class="container my-4">
-        <h1 style="text-align: center; margin-bottom:40px; text-decoration:underline">Accident Detection and Notification System</h1>
+        <h1 style="text-align: center; margin-bottom:40px; text-decoration:underline">Accident Detection and
+            Notification System</h1>
         <h4 style="
-            padding: 5px;
+            padding: 9px;
             background: #343a40;
             color: white;
             margin: 0;
             text-align: center;">
-            Recent Accident Notifications</h4>
+            Recent Accident Notifications <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bell"
+                fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="margin-bottom: 3px;font-size: 31px;">
+                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z" />
+                <path fill-rule="evenodd"
+                    d="M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
+            </svg></h4>
+
         <table class="table table-bordered">
             <thead class="thead-dark">
                 <tr>
@@ -185,7 +192,7 @@
                     <td>@mdo</td>
                     <td>@mdo</td>
                 </tr>
-               
+
             </tbody>
         </table>
 
